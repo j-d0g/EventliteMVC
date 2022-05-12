b3f74038465cecc5d6e3ec3bedc90dda90ef6be6
Jordan Tran <jordan.tran@student.manchester.ac.uk>
2022-05-12 16:49:57 +0100

    implement homepage controller

---
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/HomeController.java
@@ -0,0 +1,33 @@
+package uk.ac.man.cs.eventlite.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import uk.ac.man.cs.eventlite.dao.EventService;
+import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Event;
+import uk.ac.man.cs.eventlite.entities.Venue;
+
+@Controller
+@RequestMapping(value = "/", produces = { MediaType.TEXT_HTML_VALUE })
+public class HomeController {
+	
+	@Autowired
+	private EventService eventService;
+	@Autowired
+	private VenueService venueService;
+	
+	@GetMapping
+	public String getHome(Model model) {
+		model.addAttribute("events", eventService.getNextThree());
+		model.addAttribute("venues", venueService.findByMostEvents());
+		return "/";
+	}
+}
